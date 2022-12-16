a11318d73f789c587c464673fb0f9f2d083bc8e0
Fabian Martin <hello@fzbian.me>
2022-12-16 11:45:06 -0500

    adding parameters to the database connection

--- database/connection.go
+++ database/connection.go
@@ -2,19 +2,33 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func Connect() *sql.DB {
+	env := godotenv.Load()
+	if env != nil {
+		log.Fatalf("err loading: %v", env)
+	}
+
 	var (
 		db  *sql.DB
 		err error
 	)
 
+	dbParams := fmt.Sprintf("%s:%s@tcp(%s)/%s",
+		os.Getenv("DATABASE_USERNAME"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_NAME"))
+
 	// We make connection to the database by mysql giving the appropriate parameters
-	db, err = sql.Open("mysql", "root:@/grasfor")
+	db, err = sql.Open("mysql", dbParams)
 	if err != nil {
 		log.Fatal(err)
 	}
